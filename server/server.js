7963e71677b2687a47fc4dabbebcd49d1bb2b270
vasilena14 <vassilena.v14@gmail.com>
2021-04-28 23:02:33 +0300

    Initial Setup

---
+++ server/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import config from "./../config/config";
+import app from "./express";
+import path from "path";
+import template from "./../template";
+import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
+
+//this line is only meant for development and should be
+//commented out when building the application for production
+import devBundle from "./devBundle";
+
+// const app = express();
+
+//this line is only meant for development and should be
+//commented out when building the application for production
+devBundle.compile(app);
+
+const CURRENT_WORKING_DIR = process.cwd();
+app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
+
+app.get("/", (req, res) => {
+  res.status(200).send(template());
+});
+
+// let port = process.env.PORT || 3000;
+// app.listen(port, function onStart(err) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.info("Server started on port %s.", port);
+// });
+
+app.listen(config.port, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.info("Server started on port %s.", config.port);
+});
+
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017/studyroad";
+MongoClient.connect(url, (err, db) => {
+  console.log("Connected successfully to mongodb server");
+  db.close();
+});
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("error", () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`);
+});
---
+++ template.js
@@ -0,0 +1,27 @@
+// export default () => {
+//   return `<!doctype html>
+//     <html lang="en">
+//       <head>
+//         <meta charset="utf-8">
+//         <title>StudyRoad</title>
+//       </head>
+//       <body>
+//         <div id="root"></div>
+//         <script type="text/javascript" src="/dist/bundle.js">
+//         </script>
+//       </body>
+//     </html>`;
+// };
+
+export default () => {
+  return `<!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>StudyRoad</title>
+      </head>
+      <body>
+        <div id="root">Hello World</div>
+      </body>
+    </html>`;
+};
