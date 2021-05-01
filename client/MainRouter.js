f4f700919f5057ae3a37b65b027ede636853da1d
vasilena14 <vassilena.v14@gmail.com>
2021-05-01 21:40:38 +0300

    Created New Course component

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -8,6 +8,7 @@ import EditProfile from "./user/EditProfile";
 import Profile from "./user/Profile";
 import PrivateRoute from "./auth/PrivateRoute";
 import Menu from "./core/Menu";
+import NewCourse from "./course/NewCourse";
 
 const MainRouter = () => {
   return (
@@ -20,6 +21,8 @@ const MainRouter = () => {
         <Route path="/signin" component={Signin} />
         <PrivateRoute path="/user/edit/:userId" component={EditProfile} />
         <Route path="/user/:userId" component={Profile} />
+
+        <PrivateRoute path="/teach/course/new" component={NewCourse} />
       </Switch>
     </div>
   );
