eb30ddd60f8150ad4501a37319ceb26503f4a063
vasilena14 <vassilena.v14@gmail.com>
2021-05-01 17:52:09 +0300

    Added missing imports

--- client/user/EditProfile.js
+++ client/user/EditProfile.js
@@ -7,6 +7,8 @@ import {
   TextField,
   Typography,
   Icon,
+  FormControlLabel,
+  Switch,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import auth from "./../auth/auth-helper";
