cd93e40220cdfc7cded902ad8953f7413469d653
vasilena14 <vassilena.v14@gmail.com>
2021-05-06 22:50:00 +0300

    Created the Enroll component

---
+++ client/enrollment/Enroll.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import PropTypes from "prop-types";
+import { create } from "./api-enrollment";
+import auth from "./../auth/auth-helper";
+import { Redirect } from "react-router-dom";
+
+export default function Enroll(props) {
+  const jwt = auth.isAuthenticated();
+  const [values, setValues] = useState({
+    enrollmentId: "",
+    error: "",
+    redirect: false,
+  });
+
+  const clickEnroll = () => {
+    create(
+      {
+        courseId: props.courseId,
+      },
+      {
+        t: jwt.token,
+      }
+    ).then((data) => {
+      console.log(data);
+      if (data && data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({ ...values, enrollmentId: data._id, redirect: true });
+      }
+    });
+  };
+
+  if (values.redirect) {
+    return <Redirect to={"/learn/" + values.enrollmentId} />;
+  }
+
+  return (
+    <Button variant="contained" color="secondary" onClick={clickEnroll}>
+      Enroll
+    </Button>
+  );
+}
+
+Enroll.propTypes = {
+  courseId: PropTypes.string.isRequired,
+};
