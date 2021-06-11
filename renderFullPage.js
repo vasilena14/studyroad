5aeeea0628f439cc7cfa62242e2c6bf2fe8b7a11
vasilena14 <vassilena.v14@gmail.com>
2021-06-12 00:16:19 +0300

    Change Studyroad to StudyRoad

--- renderFullPage.js
+++ renderFullPage.js
@@ -4,7 +4,7 @@ export default ({ html, css }) => {
       <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Studyroad</title>
+        <title>StudyRoad</title>
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet">
         <style>
